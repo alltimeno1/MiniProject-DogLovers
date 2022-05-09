1b9509e378806439cbeceea0aa46f5a614232c6d
kvv(Kevin Junho Kim) <59503331+kevinkim910408@users.noreply.github.com>
2022-05-09 03:54:41 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# 01-miniProject
+ Mini Project - 항해 7기 A반 10조
---
+++ app.py
@@ -0,0 +1,34 @@
+# flask 패키지
+from flask import Flask, render_template, jsonify, request
+
+app = Flask(__name__)
+
+# mongoDB - pymongo, dnspython 패키지
+from pymongo import MongoClient
+
+# 스크래핑 - requests, bs4 패키지
+import requests
+from bs4 import BeautifulSoup
+
+# 몽고DB 연결
+client = MongoClient('mongodb+srv://test:sparta@cluster0.m7jzf.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db = client.dbsparta
+
+
+# 기본 메인 페이지 - index.html
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+
+
+
+
+
+
+
+
+# 5000 포트 사용
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
